8b6fad95f951b3cd75a190f3355d6a58adaca61e
Falah Hassan <falah.cs79@student.uomosul.edu.iq>
2021-01-17 14:10:49 +0300

    add 1D Array

---
+++ Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // Create an 1D Array 
+            int [] array1=new int[5]{1,2,3,4,5};
+            int [] array2={1,2,3,4,5};
+         //Access the Elements of an Array
+            Console.WriteLine("element of index 0 is :{0}",array1[0]);
+         //Array Length
+        Console.WriteLine("array1 length is:{0}",array2.Length);
+        //Display array using for loop
+        for (var i = 0; i < array1.Length; i++)
+        {
+            Console.WriteLine("element of index {0} in array1 is {1}",i,array1[i]);
+        }
+        //Display array using foreach loop
+        foreach (var item in array2)
+        {
+            Console.WriteLine(item);
+            
+        }
+        //user input array
+        for (var i = 0; i < array1.Length; i++)
+        {
+            Console.WriteLine("Enter element of index {0} :",i);
+            array1[i]=int.Parse(Console.ReadLine());
+        }
+        
+
+        }
+    }
+}
