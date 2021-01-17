e6a8f2dd3cff9fbfbce8b798b0ff7a956056c20d
Falah Hassan <falah.cs79@student.uomosul.edu.iq>
2021-01-17 14:50:19 +0300

    jagged Array

---
+++ jaggedArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace jaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Create an 1jagged Array 
+            var array=new int [3][];
+            array[0]=new int[6]{0,0,0,0,0,0};
+            array[1]=new int[5]{1,1,1,1,1};
+            array[2]=new int[7]{2,2,2,2,2,2,2};
+
+            for (var i = 0; i < 3; i++)
+            {
+                foreach (var item in array[i])
+                {
+                    Console.WriteLine(item);
+                    
+                    
+                }
+            }
+        }
+    }
+}
